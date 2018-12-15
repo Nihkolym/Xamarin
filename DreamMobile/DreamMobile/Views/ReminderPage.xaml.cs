f8c69833d64b265b792eac9b1a9591a2bb1620af
Bohdan Solovyov <solovey2wingo@mail.ru>
2018-12-15 06:13:48 +0200

    added simple reminder

--- DreamMobile/DreamMobile/Views/ReminderPage.xaml.cs
+++ DreamMobile/DreamMobile/Views/ReminderPage.xaml.cs
@@ -1,28 +1,31 @@
 ﻿using DreamMobile.ViewModels;
 using Plugin.LocalNotifications;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Timers;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.Threading;
 
 namespace DreamMobile.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class ReminderPage : ContentPage
-	{
-		public ReminderPage ()
-		{
-			InitializeComponent ();
-		}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ReminderPage : ContentPage
+    {
+        public ReminderPage()
+        {
+            InitializeComponent();
+        }
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-             CrossLocalNotifications.Current.Show($"HealthyDream Reminds you", $"current temperature have to be " +
-                 $"{TipsViewModel.DesiresTemperature} , yourr temperature is {TemperatureViewModel.Temperature}.", 0 , DateTime.Now.AddSeconds(1));
+            int period = RemindEntry == null ? 10000 : int.Parse(RemindEntry.Text) * 1000;
+            Timer timer = new Timer(Notife, null, 0, period);
+
+        }
+        private void Notife(Object obj)
+        {
+            CrossLocalNotifications.Current.Show($"HealthyDream Reminds you", $"current temperature have to be " +
+                  $"{TipsViewModel.DesiresTemperature} , yourr temperature is {TemperatureViewModel.Temperature}.");
+
         }
     }
 }
