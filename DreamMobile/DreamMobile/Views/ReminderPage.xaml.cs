fab562d63ebd9406ab2fa82c39ae7671cb4122f7
Bohdan Solovyov <solovey2wingo@mail.ru>
2018-12-15 16:57:36 +0200

    notification added

--- DreamMobile/DreamMobile/Views/ReminderPage.xaml.cs
+++ DreamMobile/DreamMobile/Views/ReminderPage.xaml.cs
@@ -10,6 +10,8 @@ namespace DreamMobile.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ReminderPage : ContentPage
     {
+        private Timer timer;
+
         public ReminderPage()
         {
             InitializeComponent();
@@ -17,15 +19,20 @@ namespace DreamMobile.Views
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-            int period = RemindEntry == null ? 10000 : int.Parse(RemindEntry.Text) * 1000;
-            Timer timer = new Timer(Notife, null, 0, period);
+            int period = RemindEntry.Text == null ? 10000 : int.Parse(RemindEntry.Text) * 1000;
+            timer = new Timer(Notife, null, 0, period);
 
         }
         private void Notife(Object obj)
         {
             CrossLocalNotifications.Current.Show($"HealthyDream Reminds you", $"current temperature have to be " +
-                  $"{TipsViewModel.DesiresTemperature} , yourr temperature is {TemperatureViewModel.Temperature}.");
+                  $"{TipsViewModel.DesiresTemperature} , yourr temperature is {TemperatureViewModel.Temperature}.", id: 101);
+        }
 
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            CrossLocalNotifications.Current.Cancel(101);
+            timer.Dispose();
         }
     }
 }
