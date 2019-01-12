c05db5d585e4027c567dd0c61961f4d3f65ca258
Sergey Palamarchuk <serega217348@gmail.com>
2019-01-13 01:54:49 +0200

    Implementation of localization

--- DreamMobile/DreamMobile/App.xaml.cs
+++ DreamMobile/DreamMobile/App.xaml.cs
@@ -4,6 +4,10 @@ using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using DreamMobile.Helpers;
+using System.Reflection;
+using DreamMobile.Services;
+using DreamMobile.Localization.Resources;
+using System.Threading;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace DreamMobile
@@ -11,12 +15,13 @@ namespace DreamMobile
     public partial class App : Application
     {
 
-  
+
 
         public App()
         {
             InitializeComponent();
 
+            SetupLocalization();
 
             if (Settings.AccessToken != "")
             {
@@ -25,6 +30,24 @@ namespace DreamMobile
             else
             {
                 MainPage = new NavigationPage(new RegisterPage());
+            }
+        }
+
+        private static void SetupLocalization()
+        {
+            System.Diagnostics.Debug.WriteLine("====== resource debug info =========");
+            var assembly = typeof(App).GetTypeInfo().Assembly;
+            foreach (var res in assembly.GetManifestResourceNames())
+                System.Diagnostics.Debug.WriteLine("found resource: " + res);
+            System.Diagnostics.Debug.WriteLine("====================================");
+
+            // This lookup NOT required for Windows platforms - the Culture will be automatically set
+            if (Device.RuntimePlatform == Device.iOS || Device.RuntimePlatform == Device.Android)
+            {
+                // determine the correct, supported .NET culture
+                var ci = DependencyService.Get<ILocalize>().GetCurrentCultureInfo();
+                DreamMobile.Localization.Resources.Resources.Culture = ci; // set the RESX for resource localization
+                DependencyService.Get<ILocalize>().SetLocale(ci); // set the Thread for locale-aware methods
             }
         }
 
